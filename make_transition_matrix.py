993d4ba7d0e9df61ae29ecbd7ab760d106c186b7
DaniyalAbbasi <36009198+abbasidaniyal@users.noreply.github.com>
2019-10-16 01:22:19 +0530

    Midi lib changed. Params to be decided

--- make_transition_matrix.py
+++ make_transition_matrix.py
@@ -3,20 +3,18 @@ import numpy as np
 import pandas as pd
 import read_mid as rm
 
+
 def make_transition(file_path):
     df = np.zeros(shape=(128, 128))
 
-
     df = pd.DataFrame(df)
 
-
     result = rm.parse_mid(file_path)
-
 
     for i in range(len(result["pitch"])-1):
         df[result["pitch"][i]][result["pitch"][i+1]] += 1
 
-
+    # Jupyter Notebook Specifications
     # pd.option_context('display.max_columns', None)
     # # pd.options.display
     # pd.options.display.max_rows = 4000
@@ -28,10 +26,9 @@ def make_transition(file_path):
 
 
 if __name__ == "__main__":
-    
-    if len(sys.argv)<2:
+
+    if len(sys.argv) < 2:
         print("No file path specified")
     else:
         for i in range(1, len(sys.argv)):
             print(make_transition(sys.argv[i]))
-
